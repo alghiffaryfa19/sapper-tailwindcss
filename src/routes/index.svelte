ad72df3853778418467273e4a4000cfd38981e29
Fauzan Amir Al Ghiffary <fauzanamiralghiffary@MacBook-Pro-Fauzan.local>
2021-04-22 18:38:00 +0700

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,56 @@
+<script>
+	import successkid from 'images/successkid.jpg';
+	import { onMount } from "svelte";
+	onMount(async () => {
+		const { default: tippy } = await import("tippy.js");
+		tippy('[data-tippy-content]');
+	});
+</script>
+
+<style global>
+	@import 'tippy.js/dist/tippy.css';
+	h1, figure, p {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	figure {
+		margin: 0 0 1em 0;
+	}
+
+	img {
+		width: 100%;
+		max-width: 400px;
+		margin: 0 0 1em 0;
+	}
+
+	p {
+		margin: 1em auto;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
+
+<svelte:head>
+	<title>Sapper project template</title>
+</svelte:head>
+
+<h1 data-tippy-content="Hello World">Great success!</h1>
+
+<figure>
+	<img alt="Success Kid" src="{successkid}">
+	<figcaption>Have fun with Sapper!</figcaption>
+</figure>
+
+<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
